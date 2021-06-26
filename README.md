d76c115a0561c5b68403fe1615b1f3336dc5ef41
Tranthehong <tranthehong111290@gmail.com>
2021-06-26 13:20:54 +0700

    update auth

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Fullstack thương mại điện tử.
+# Fullstack thương mại điện tử reactjs-redux-express-mongoose.
 
 ## BACKEND:
-# Thietlap
 npm init
 npm install --save express express-validator mongoose dotenv 
 npm install --save-dev nodemon
 npm install --save cors
 npm install --save jsonwebtoken
+npm install --save shortid
 
 # START SERVER
 npm start
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,60 +1,75 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
-require('dotenv').config();
+require('dotenv').config()
+const shortid = require('shortid')
 
 
+//Đăng kí mới
 exports.signup = (req, res) => {
-    User.findOne({ email: req.body.email })
-        .exec((error, user) => {
-            if (user) return res.status(400).json({
-
-                message: `Loi ton tai user ${error}`
-            });
-            const { firstName, lastName, email, password } = req.body;
-            const _user = new User({
-                firstName,
-                lastName,
-                email,
-                password,
-                userName: Math.random(111)
-            });
-
-            _user.save((error, data) => {
-                if (error) {
-                    return res.status(400).json({
-                        message: `Loi ${error}`
-                    })
-                }
-                if (data) {
-                    return res.status(201).json({
-                        user: data
-                    })
-                }
-            });
+    User.findOne({ email: req.body.email }).exec(async (error, user) => {
+        //Trường hợp có lỗi.
+        if (error) return res.status(400).json({ message: "Có lỗi khi xác thực thông tin user" });
+        //Trường hợp đã tồn tại user name
+        if (user) return res.status(400).json({ message: "User đã tồn tại" })
+        //Lấy thông tin từ client
+        const { firstName, lastName, email, password } = req.body
+        //Mã hóa lại mật khẩu
+        const hash_password = await bcrypt.hash(password, 10);
+        //Tạo một đối tượng User từ thông tin lấy được từ client
+        const userTemp = new User({
+            firstName,
+            lastName,
+            email,
+            password,
+            username: shortid.generate(),
         });
+        //Lưu lại thông tin user
+        userTemp.save((error, user) => {
+            //Trường hợp lỗi
+            if (error) {
+                return res.status(400).json({ message: "Tạo user thất bại" });
+            }
+            //Trường hợp lưu thành công.
+            if (user) {
+                const token = generateJwtToken(user._id, user.role);
+                const { _id, firstName, lastName, email, role, fullName } = user;
+                return res.status(201).json({
+                    token,
+                    user: { _id, firstName, lastName, email, role, fullName },
+                });
+            }
+        })
+    });
 }
 
+//Check login
 exports.signin = (req, res) => {
-    console.log("ss")
-    User.findOne({ email: req.body.email })
-        .exec((error, user) => {
-            if (error) return res.status(400).json({ error });
-            if (user) {
-                if (user.authentication(req.body.password)) {
-                    const token = jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-                    const { firstName, lastName, email, role, fullname } = user;
-                    res.status(200).json({
-                        token,
-                        user: {
-                            firstName, lastName, email, role, fullname
-                        }
-                    })
-                } else {
-                    return res.status(400).json({ message: "Sai username or passowrd" })
-                }
-
+    User.findOne({ email: req.body.email }).exec(async (error, user) => {
+        if (error) return res.status(400).json({ error });
+        if (user) {
+            const isPassword = await user.authentication(req.body.password)
+            if (isPassword && user.role === "user") {
+                const token = generateJwtToken(user._id, user.role);
+                const { _id, firstName, lastName, email, role, fullname } = user;
+                res.status(200).json({
+                    token,
+                    user: {
+                        _id, firstName, lastName, email, role, fullname
+                    }
+                });
             } else {
                 return res.status(400).json({ message: "Something went wrong" })
             }
-        });
+
+        } else {
+            return res.status(400).json({ message: "Something went wrong" })
+        }
+    });
 }
+
+//Tạo tokenid
+const generateJwtToken = (_id, role) => {
+    return jwt.sign({ _id, role }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    });
+};
